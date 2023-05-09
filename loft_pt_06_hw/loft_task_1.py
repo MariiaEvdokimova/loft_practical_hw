ea2c67f866a943ee5057e394ca5ab26c2f4fc977
Mariia Evdokimova <evdokimovamariam@gmail.com>
2023-05-10 00:51:15 +0300

    update

--- loft_pt_06_hw/loft_task_1.py
+++ loft_pt_06_hw/loft_task_1.py
@@ -20,10 +20,23 @@
 <function_name>(-2,14) -> [-2, 0, 2, 4, 6, 8, 10.0, 12.0, 14.0]
 <function_name>(1,-10) -> ValueError"""
 
-def main(begin: int, end : int) -> list:
+def main(begin: int, end: int) -> list:
     if begin > end:
         raise ValueError
-    return [num for num in range(begin, end+1) if num %2 == 0]
+    return [num for num in range(begin, end+1) if num % 2 == 0]
+
+print(main(1,10))
+print(main(1,13))
+print(main(-2,14))
+print(main(1,-10))
+
+#Усложнение #1 для Задания #6.1
+def main(begin: int, end: int) -> list:
+    if begin > end:
+        raise ValueError
+    rez = [num for num in range(begin, end+1) if num % 2 == 0]
+    rez2 = [float(num) if num > (end - begin) / 2 else num for num in [num for num in range(begin, end+1) if num % 2 == 0]]
+    return rez2
 
 print(main(1,10))
 print(main(1,13))
